517080719f55d37b9c5ba6a90572539f5bc20d82
eangyao <ang.yao@ericsson.com>
2019-07-19 19:01:42 +0800

    query example

---
+++ query_auth.fetch.js
@@ -0,0 +1,35 @@
+let graphQLParams = {
+	"query":`query searchAuthor($id:Int! = 1){
+		 hello {
+			  id
+			  firstName
+			  lastName
+			  }
+		 authors {
+			  id
+			  firstName
+			  }
+		 
+		 author(id:$id) {
+			  id
+			  firstName
+			  }
+		​
+}
+`,
+	"variables":{"id":33},
+	"operationName":"searchAuthor"
+}
+
+fetch('http://localhost:4000/graphql?', {
+	        method: 'post',
+	        headers: {
+			          'Accept': 'application/json',
+			          'Content-Type': 'application/json'
+			        },
+	        body: JSON.stringify(graphQLParams),
+	        credentials: 'include',
+	      }).then(function (response) {
+		              response.json().then(data=>console.log(data))
+		            });
+
